82f3ac618e8a1bd6488408174318f351a8a88b9c
VaishaliRakshe <vaishalijadhav000@gmail.com>
2022-01-04 18:32:04 -0600

    update

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,8 +17,27 @@ namespace TechJobsMVC.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.searchType = "all";
             return View();
         }
-        
+        [HttpPost("/Search/Results")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+            if (String.IsNullOrEmpty(searchType) || String.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            ViewBag.searchType = searchType;
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+            return View("Index");
+        }
+
     }
 }
